fe8a210aa6b4cb41c7a29338746f2cdf29a94830
harshabapatla <hvardhan0098@gmail.com>
2023-07-14 16:23:33 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
+
+
+const app = express();
+
+
+const items =["Buy Food","Cook Food","Eat Food"];
+let workItems= [];
+
+
+app.set('view engine', 'ejs');
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({
+extended:true
+}));
+
+app.get("/", function (req, res) {
+
+    const day = date.getDate();
+    res.render("list", { listTitle: day, newListItems : items });
+   
+});
+
+app.post("/",function(req,res){
+    let item = req.body.newItem;
+
+    if (req.body.list ==="Work"){
+
+        workItems.push(item);
+        res.redirect("/work");
+    }
+    else{
+    items.push(item);
+    res.redirect("/");
+    }
+});
+
+app.get("/work", function(req,res){
+    res.render("list", {listTitle : "Work List",
+    newListItems :workItems})
+});
+app.get("/about",function(req,res){
+    res.render("about");
+})
+
+app.listen(3000, function () {
+    console.log("Server started on 3000");
+});
